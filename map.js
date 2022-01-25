5a4c675ed2ea9f099b9bb5fe4aea93e06c0838d6
Dabronzo <ayrtonterceiro@gmail.com>
2022-01-25 12:24:23 +0000

    Finished Map method

--- map.js
+++ map.js
@@ -5,15 +5,44 @@
  */
 
 // Using a for loop
+let nums = [1, 2, 3, 4, 5]
 
+let results = [];
+
+for (let num of nums){
+  results.push(num*2);
+}
+
+console.log(results);
 
 // Using map()
 
 
+
+//defining a function to multiply by two
+
+const multiplyBy2 = function(num) {
+  return num*2;
+
+}
+
+//calling the map method on the array
+
+const mapResults = nums.map(multiplyBy2);
+console.log(mapResults);
+
+
+
+
+
 // Simplified w/ map()
 
-
 // Simplfied w/ map() + arrow function
+const mapSimples = nums.map(num => num * 2);
+
+console.log("Simplefied map :", mapSimples);
+
+
 
 
 // With objects:
@@ -37,3 +66,17 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+//returnning as an array:
+
+//const result = students.map(student => [student.name, student.id]);
+
+//console.log(result);
+
+//returning as objects
+
+//const resultObjects = students.map(student => [{student: student.name, id: student.id}]);
+
+//console.log(resultObjects);
+
+//adding an age to each student
